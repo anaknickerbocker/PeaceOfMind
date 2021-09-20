98645fc60fca088c95289a2716e2b601be2c1a34
Ana Knickerbocker <akknickerbocker@spscommerce.com>
2021-09-20 17:48:10 -0500

    Use rethinkdb instead of prisma
    Update database host

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,23 +1,30 @@
-import * as express from 'express';
-// import { Message } from '@peace-of-mind/api-interfaces';
-import { PrismaClient } from '@prisma/client';
-import * as path from 'path';
-import * as dotenv from 'dotenv';
+import express from 'express';
+import path from 'path';
+import dotenv from 'dotenv';
+import r from 'rethinkdb';
 // import router from './routes/router';
+// import { Message } from '@peace-of-mind/api-interfaces';
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
 
-const prisma = new PrismaClient();
+r.connect({ host: process.env.DATABASE_URL, port: 28015 }, (err, conn) => {
+  if (err) throw err;
+  r.table('tv_shows')
+    .insert({ name: 'Breaking Bad' })
+    .run(conn, function (err, res) {
+      if (err) throw err;
+      console.log(res);
+    });
+});
+
 const app = express();
 const CLIENT_BUILD_PATH = path.join(__dirname, '../peace-of-mind');
 app.use(express.static(CLIENT_BUILD_PATH));
-// const greeting: Message = { message: 'Welcome to api!' };
 
 app.get('/api', async (req, res) => {
-  const allUsers = await prisma.users.findMany({});
-  res.send(allUsers);
+  res.status(200);
 });
 
 app.get('*', (req, res) => {
