6d690210dfa2297bbff62d33c7cd214ac56c9bed
Ana Knickerbocker <akknickerbocker@spscommerce.com>
2021-09-21 15:01:43 -0500

    Make the login page work

--- apps/peace-of-mind/src/app/components/Login/Login.tsx
+++ apps/peace-of-mind/src/app/components/Login/Login.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import './Login.css';
+import PropTypes from 'prop-types';
+
+async function loginUser(credentials: { username: string; password: string }) {
+  return fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+}
 
 export default function Login(props: {
-  setToken: React.Dispatch<React.SetStateAction<string>>;
+  setToken: (userToken: { token: string }) => void;
 }) {
+  const [username, setUserName] = useState<string | undefined>();
+  const [password, setPassword] = useState<string | undefined>();
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    if (username?.length && password?.length) {
+      e.preventDefault();
+      const token = await loginUser({
+        username,
+        password,
+      });
+      props.setToken(token);
+    }
+  };
+
   return (
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" />
+          <input type="text" onChange={(e) => setUserName(e.target.value)} />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <div>
           <button type="submit">Submit</button>
@@ -23,3 +51,7 @@ export default function Login(props: {
     </div>
   );
 }
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired,
+};
