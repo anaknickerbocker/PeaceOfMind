7b446a5d9922f37437ebdb9e98fbf306f498cd98
Jenna Truong <jktruong@spscommerce.com>
2021-09-22 10:43:29 -0500

    user profile page basics

--- apps/peace-of-mind/src/app/components/Profile.tsx
+++ apps/peace-of-mind/src/app/components/Profile.tsx
@@ -1,5 +1,54 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
-export default function Profile() {
-  return <h2>User Profile Page</h2>;
-}
+const Profile = () => {
+  const [firstName, setFirstName] = useState<string | undefined>();
+  const [lastName, setLastName] = useState<string | undefined>();
+  const [contactMethod, setContactMethod] = useState<string | undefined>();
+  const [contactDetails, setContactDetails] = useState<string | undefined>();
+
+  return (
+    <div className="profile-wrapper">
+    <h2>User Profile Page</h2>
+    <p>Please create your user profile</p>
+      <form>
+        <label>
+          <p>First Name</p>
+          <input
+            type="firstName"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </label>
+        <label>
+          <p>Last Name</p>
+          <input
+            type="lastName"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </label>
+        <label>
+          <p>Prefered Contact Method</p>
+          <select 
+            name="selectContactMethod" 
+            id="selectList"
+            onChange={(e) => setContactMethod(e.target.value)}>
+            <option value="option 1">Text Message</option>
+            <option value="option 2">Email</option>
+          </select>
+        </label>
+        <label>
+          <p>Contact Details</p>
+          <input
+            type="contactDetails"
+            placeholder="Enter Phone or Email"
+            onChange={(e) => setContactDetails(e.target.value)}
+          />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Profile;
